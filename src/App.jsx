449ec20473364d591c0216dc743b66d98eaaa1da
MRifqiandi <11211063@student.itk.ac.id>
2024-07-24 22:20:54 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/navbar" element={<Navbar />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
